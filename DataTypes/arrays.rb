e7b7c85855562926d83fba4d5cc7e1213273ed8f
natew8 <n8w8dev@gmail.com>
2021-04-27 09:13:32 -0600

    Arrays

--- DataTypes/arrays.rb
+++ DataTypes/arrays.rb
@@ -1,12 +1,27 @@
 # an array is a way to store multiple like values
 # TO CREATE AN array
-friends = Array["Cory", "Kaylie", "Bash"]
+friends = Array.new
+
+# You can find the value at indexes just like javascript
+#   friends[1] = Kaylie
+# BUT you can find values counting from the end of the array moving backwards. 
+#   friends[-1] = 'Bash'
+#   friends[-2] = 'Kaylie'
+# YOU can also find a range of values 
+#   friends[0, 2]
+# this will do from index 0 up to BUT NOT INCLUDING index 2
+
 
 # You can use push just like in javascrip
-friends.push("Nate")
-# you can also use pop
-friends.pop()
+friends.push("Nate", "Kaylie", "Bash", "Lucas")
+# you can also use length
+puts friends.length()
+# You can check if a value is in an array
+puts friends.include? "Kaylie"
+# You can reverse the array 
+puts friends.reverse()
+# you cabn sort the array if it has like value types
+puts friends.sort()
 
-friends.slice(1,1)
 
 puts friends
